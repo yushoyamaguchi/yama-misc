24d3efcf3153105dc0e4976d7a9538b1b527e84f
yushoyamaguchi <ysh.824@outlook.jp>
2024-08-24 16:22:46 +0900

    lb

---
+++ kind-playground/cilium_lb/install_cilium.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -euo pipefail
+
+KUBE_SYSTEM_NAMESPACE="kube-system"
+
+CILIUM_NAMESPACE="${KUBE_SYSTEM_NAMESPACE}"
+CILIUM_VERSION="1.15.4"
+CLUSTER_NAME_PREFIX="cl"
+CLUSTER_1_NAME="${CLUSTER_NAME_PREFIX}1"
+CLUSTER_1_CONTEXT="kind-${CLUSTER_1_NAME}"
+
+
+
+function info() {
+    echo "=> ${1}"
+}
+
+
+
+info "Creating the clusters..."
+kind create cluster --name "${CLUSTER_1_NAME}" --config "${CLUSTER_1_NAME}/kind.yaml"
+
+info "Building and loading the docker images..."
+docker build -t simple-web1 ./cl1
+kind load docker-image simple-web1 --name "${CLUSTER_1_NAME}"
+
+info "Installing Cilium...."
+cilium install --version "${CILIUM_VERSION}" \
+    --set ipam.mode=kubernetes \
+    --set hubble.ui.enabled=true \
+    --set hubble.relay.enabled=true \
+    --set kubeProxyReplacement=strict \
+    --set bgpControlPlane.enabled=true \
+    --set k8s.requireIPv4PodCIDR=true \
+    --set externalIPs.enabled=true
+
+
