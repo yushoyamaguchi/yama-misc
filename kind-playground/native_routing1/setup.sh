3dbe08d5479886888b90d6be60a2f335578456d3
yushoyamaguchi <ysh.824@outlook.jp>
2024-09-12 14:42:59 +0900

    cilium config flag

--- kind-playground/native_routing1/setup.sh
+++ kind-playground/native_routing1/setup.sh
@@ -4,7 +4,7 @@ docker build -t custom-kind-node:v1.29.0-network-tools ./kind-image
 
 kind create cluster --config=kind-config.yaml
 
-cilium install --namespace kube-system --config cilium-config.yaml
+cilium install --namespace kube-system --values cilium-config.yaml
 
 cilium status --wait > /dev/null 2>&1
 
