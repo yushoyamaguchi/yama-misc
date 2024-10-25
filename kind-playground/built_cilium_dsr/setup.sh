214aa612d6ea809b2c4aa6d42d0beb903d03afe9
Yusho Yamaguchi <yusho.yamaguchi@sony.com>
2024-10-25 20:03:58 +0900

    use container image built by myself

--- kind-playground/built_cilium_dsr/setup.sh
+++ kind-playground/built_cilium_dsr/setup.sh
@@ -3,6 +3,7 @@
 # Default value
 DEFAULT_CILIUM_DIR="/home/y-yamaguchi/yusho/cilium"
 KIND_IMAGE="custom-kind-node:v1.29.0-network-tools2"
+NETSHOOT_NGINX_IMAGE="my-netshoot-nginx:v1"
 
 # Check if CILIUM_DIR is already set in the environment
 if [ -z "${CILIUM_DIR}" ]; then
@@ -20,8 +21,14 @@ echo "Setting up Cilium with Geneve on Kind"
 echo "building custom kind node image with network tools"
 docker build --network=host -t $KIND_IMAGE ./kind-image
 
+echo "building netshoot & nginx images"
+docker build --network=host -t $NETSHOOT_NGINX_IMAGE ./pod-image
+
 echo "creating kind cluster"
 kind create cluster --image $KIND_IMAGE --config=kind-config.yaml
+
+echo "loading netshoot & nginx images into kind cluster"
+kind load docker-image $NETSHOOT_NGINX_IMAGE --name kind
 
 echo "building cilium image"
 make -C "${CILIUM_DIR}" kind-image
