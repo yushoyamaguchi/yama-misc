b709334fb58e4cdd8a464d32e4c44a7ea2bb3398
yushoyamaguchi <ysh.824@outlook.jp>
2024-11-20 23:21:48 +0900

    latest frr

--- linux_net/mcast-frr/setup.sh
+++ linux_net/mcast-frr/setup.sh
@@ -3,7 +3,9 @@
 sudo pwd
 
 IMAGE_NAME="yamaguchi-frr:mcast1"
+LATEST_IMAGE_NAME="yamaguchi-frr-latest:mcast1"
 
-docker build -t $IMAGE_NAME .
+#docker build -t $IMAGE_NAME .
+docker build -t $LATEST_IMAGE_NAME ./latest-frr
 
 sudo containerlab -t lab.yaml deploy
