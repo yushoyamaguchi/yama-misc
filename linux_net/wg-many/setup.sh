9b476bdf6e977bfa4fa04d1fd7588d7daebf8c63
Yusho Yamaguchi <yusho.yamaguchi@sony.com>
2024-12-04 12:18:37 +0900

    a

--- linux_net/wg-many/setup.sh
+++ linux_net/wg-many/setup.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <number>"
+  exit 1
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+  echo "Error: Argument must be a number."
+  exit 1
+fi
+
+if [ $1 -lt 2 ] || [ $1 -gt 254 ]; then
+  echo "Error: Number must be between 2 and 254."
+  exit 1
+fi
+
 mkdir -p auto-gen
 
-bash ./inventory_ini.sh 3
+bash ./inventory_ini.sh $1
+bash ./generate_playbook.sh $1
 
 
 cp ./wg0_node.conf.j2 auto-gen/wg0_node.conf.j2
