3472747e0031b16ea87565bc859505adc387734b
Yusho Yamaguchi <yusho.yamaguchi@sony.com>
2024-12-04 18:19:54 +0900

    connection check

--- linux_net/wg-many/setup.sh
+++ linux_net/wg-many/setup.sh
@@ -10,7 +10,7 @@ if ! [[ $1 =~ ^[0-9]+$ ]]; then
   exit 1
 fi
 
-if [ $1 -lt 2 ] || [ $1 -gt 254 ]; then
+if [ $1 -lt 2 ] || [ $1 -gt 50000 ]; then
   echo "Error: Number must be between 2 and 254."
   exit 1
 fi
@@ -32,3 +32,8 @@ sudo containerlab -t auto-gen/lab.yaml deploy
 sudo ip route add 172.29.0.0/16 via 172.20.20.3
 
 ansible-playbook -i auto-gen/inventory.ini auto-gen/wireguard_setup.yml
+
+THIRD_OCTET=$(( ($1 - 1) / 250 ))
+FOURTH_OCTET=$(( ($1 - 1) % 250 + 1 ))
+
+docker exec -it clab-wg-many-server1 ping -c 3 10.0.$THIRD_OCTET.$FOURTH_OCTET
