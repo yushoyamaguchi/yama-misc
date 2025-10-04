6db9b20a956d98e7763d21f9170d0eebf724783f
YushoYamaguchi <ysh.824@outlook.jp>
2025-10-05 01:32:04 +0900

    change cilium dir

--- cilium-kind-playground/built_cilium_lb/setup.sh
+++ cilium-kind-playground/built_cilium_lb/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default value
-DEFAULT_CILIUM_DIR="/home/y-yamaguchi/yusho/cilium"
+DEFAULT_CILIUM_DIR="/srv/unbound-sub/cilium"
 KIND_IMAGE="custom-kind-node:v1.29.0-network-tools2"
 NETSHOOT_NGINX_IMAGE="my-netshoot-nginx:v2"
 
