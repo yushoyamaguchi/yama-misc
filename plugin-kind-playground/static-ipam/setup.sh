976e537112074a0c52679f77d2cad9f0658ec2f0
Yusho Yamaguchi <ysh.824@outlook.jp>
2026-04-15 11:22:00 +0900

    plugin setup

--- plugin-kind-playground/static-ipam/setup.sh
+++ plugin-kind-playground/static-ipam/setup.sh
@@ -9,4 +9,11 @@ echo "loading netshoot & nginx images into kind cluster"
 kind load docker-image $NETSHOOT_NGINX_IMAGE --name kind
 
 echo "copy cni binary to every node"
-cd load
+# ここは空でいい
+
+echo "copy conf file"
+docker cp ipvlan-worker1.conf kind-worker:/etc/cni/net.d/10-ipvlan.conf
+docker cp ipvlan-worker2.conf kind-worker2:/etc/cni/net.d/10-ipvlan.conf
+
+echo "deploy netshoot daemonset"
+kubectl apply -f netshoot-daemonset.yaml
